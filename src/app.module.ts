c15abe9cb19ced8a9deae459a6ac55c0b220f6c2
Adriel Gorosito <adrielgorosito14@gmail.com>
2025-01-21 19:26:16 -0300

    First config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      username: DB_USER,
+      password: DB_PASSWORD,
+      host: DB_HOST,
+      port: +DB_PORT,
+      database: DB_NAME,
+      entities: [__dirname + '///*.entity{.ts,.js}'],
+      synchronize: true,
+    })]
 })
 export class AppModule {}
