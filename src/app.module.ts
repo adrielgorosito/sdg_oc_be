8b713f923c75d83ac06a68862151b9c71d8df632
Adriel Gorosito <adrielgorosito14@gmail.com>
2025-01-21 20:08:23 -0300

    TypeORM config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,19 +1,28 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      username: DB_USER,
-      password: DB_PASSWORD,
-      host: DB_HOST,
-      port: +DB_PORT,
-      database: DB_NAME,
-      entities: [__dirname + '///*.entity{.ts,.js}'],
-      synchronize: true,
-    })]
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'mssql',
+        username: configService.get('DB_USER'),
+        password: configService.get('DB_PASSWORD'),
+        host: configService.get('DB_HOST'),
+        port: parseInt(configService.get('DB_PORT')),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '///*.entity{.ts,.js}'],
+        synchronize: true,
+        options: {
+          encrypt: true,
+          trustServerCertificate: true,
+        },
+      }),
+      inject: [ConfigService]
+    })
+  ]
 })
 export class AppModule {}
