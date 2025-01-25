10887021597db7dd1972e82623bda67106388beb
Adriel Gorosito <adrielgorosito14@gmail.com>
2025-01-25 03:48:57 -0300

    Obra social endpoints

--- src/obra-social/obra-social.controller.ts
+++ src/obra-social/obra-social.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ObraSocialService } from './obra-social.service';
+import { ObraSocialDTO } from './dto/obra-social.dto';
 
 @Controller('obra-social')
 export class ObraSocialController {
   constructor(private readonly obraSocialService: ObraSocialService) {}
+
+  @Get()
+  async findAll() {
+    return await this.obraSocialService.findAll();
+  }
+
+  @Get('/:id')
+  async findOne(@Param('id') id: number) {
+    return await this.obraSocialService.findOne(id);
+  }
+
+  @Post()
+  async createOne(@Body() obraSocialDTO: ObraSocialDTO) {
+    return await this.obraSocialService.create(obraSocialDTO);
+  }
+
+  @Patch('/:id')
+  async update(@Param('id') id: number, @Body() obraSocialDTO: ObraSocialDTO) {
+    return await this.obraSocialService.update(id, obraSocialDTO);
+  }
+
+  @Delete('/:id')
+  async deleteOne(@Param('id') id: number) {
+    return await this.obraSocialService.delete(id);
+  }
 }
--- src/obra-social/obra-social.module.ts
+++ src/obra-social/obra-social.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ObraSocialService } from './obra-social.service';
 import { ObraSocialController } from './obra-social.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ObraSocial } from './entities/obra-social.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([ObraSocial])],
   controllers: [ObraSocialController],
   providers: [ObraSocialService],
 })
