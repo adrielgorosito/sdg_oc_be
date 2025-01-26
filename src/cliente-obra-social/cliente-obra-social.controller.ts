5ec0475b1362a3ab05ab2c79ad0b12b59c056255
Adriel Gorosito <adrielgorosito14@gmail.com>
2025-01-26 14:17:47 -0300

    Cliente-ObraSocial entity

--- src/cliente-obra-social/cliente-obra-social.controller.ts
+++ src/cliente-obra-social/cliente-obra-social.controller.ts
@@ -1,9 +1,53 @@
-import { Controller } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
 import { ClienteObraSocialService } from './cliente-obra-social.service';
+import { ClienteObraSocialDTO } from './dto/cliente-obra-social.dto';
 
 @Controller('cliente-obra-social')
 export class ClienteObraSocialController {
   constructor(
     private readonly clienteObraSocialService: ClienteObraSocialService,
   ) {}
+
+  @Get()
+  async findAll(
+    @Query('idCliente') idCliente: number,
+    @Query('idObraSocial') idObraSocial: number,
+  ) {
+    return await this.clienteObraSocialService.findAll(idCliente, idObraSocial);
+  }
+
+  @Get('/:id')
+  async findOneById(@Param('id') id: number) {
+    return await this.clienteObraSocialService.findOneById(id);
+  }
+
+  @Post()
+  async createOne(@Body() cliObSocDTO: ClienteObraSocialDTO) {
+    return await this.clienteObraSocialService.create(cliObSocDTO);
+  }
+
+  @Patch('/:id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateNroSocio: { nroSocio: number },
+  ) {
+    return await this.clienteObraSocialService.update(
+      id,
+      updateNroSocio.nroSocio,
+    );
+  }
+
+  @Delete('/:id')
+  async deleteOne(@Param('id') id: number) {
+    return await this.clienteObraSocialService.delete(id);
+  }
 }
