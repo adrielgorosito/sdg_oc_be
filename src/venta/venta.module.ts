06ac2115fb77c287fcc2b4fd5bb0d4c867fd6fe8
Adriel Gorosito <adrielgorosito14@gmail.com>
2025-02-02 16:36:18 -0300

    Update venta.module.ts

--- src/venta/venta.module.ts
+++ src/venta/venta.module.ts
@@ -1,13 +1,13 @@
 import { Module } from '@nestjs/common';
-import { VentasService } from './venta.service';
-import { VentasController } from './venta.controller';
+import { VentaService } from './venta.service';
+import { VentaController } from './venta.controller';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Venta } from './entities/venta.entity';
 import { Cliente } from 'src/cliente/entities/cliente.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Venta, Cliente])],
-  controllers: [VentasController],
-  providers: [VentasService],
+  controllers: [VentaController],
+  providers: [VentaService],
 })
-export class VentasModule {}
+export class VentaModule {}
