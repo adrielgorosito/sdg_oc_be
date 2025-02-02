3a62d07be4525446b1b96c83ff4a58c15f1b6f90
Adriel Gorosito <adrielgorosito14@gmail.com>
2025-02-02 16:18:08 -0300

    Update create-marca.dto.ts

--- src/marca/dto/create-marca.dto.ts
+++ src/marca/dto/create-marca.dto.ts
@@ -3,11 +3,11 @@ import { BaseDTO } from 'src/common/dtos/base.dto';
 import { RelationDTO } from 'src/common/dtos/relation.dto';
 
 export class CreateMarcaDTO extends BaseDTO {
-  @IsOptional()
-  @IsArray()
-  productos: RelationDTO[];
-
   @IsNotEmpty()
   @IsString()
   nombre: string;
+  
+  @IsOptional()
+  @IsArray()
+  productos: RelationDTO[];
 }
