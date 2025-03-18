cf0f69a8182b23eb13d8bcc62eb547cb8c02dfba
BraidaFac <facundobraida98@gmail.com>
2025-03-18 14:04:21 -0300

    Implementación de la descarga de archivos en el controlador de carga y mejora en la verificación de existencia de archivos en el servicio de carga. Se agregó un nuevo archivo PDF al directorio de uploads.

--- src/common/upload/upload.controller.ts
+++ src/common/upload/upload.controller.ts
@@ -1,4 +1,5 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { UploadService } from './upload.service';
 
 @Controller('uploads')
@@ -9,7 +10,10 @@ export class UploadController {
   async findOne(
     @Param('tipo') tipo: string,
     @Param('nombre') nombreArchivo: string,
+    @Res() res: Response,
   ) {
-    return await this.uploadService.findOne(tipo, nombreArchivo);
+    const filePath = await this.uploadService.findOne(tipo, nombreArchivo);
+
+    res.sendFile(filePath);
   }
 }
