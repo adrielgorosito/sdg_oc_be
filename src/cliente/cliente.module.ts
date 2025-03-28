b0071af9890eeca4f59cd993a990ab544e6ec0a8
Adriel Gorosito <adrielgorosito14@gmail.com>
2025-03-27 21:39:32 -0300

    Fixed update and create methods where Obra Social doesn't exists

--- src/cliente/cliente.module.ts
+++ src/cliente/cliente.module.ts
@@ -3,9 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ClienteController } from './cliente.controller';
 import { ClienteService } from './cliente.service';
 import { Cliente } from './entities/cliente.entity';
+import { ClienteObraSocial } from 'src/cliente-obra-social/entities/cliente-obra-social.entity';
+import { ObraSocial } from 'src/obra-social/entities/obra-social.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Cliente])],
+  imports: [TypeOrmModule.forFeature([Cliente, ClienteObraSocial, ObraSocial])],
   controllers: [ClienteController],
   providers: [ClienteService],
 })
