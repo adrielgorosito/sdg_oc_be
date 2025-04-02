8f26d7acc3334972677669ece08ad1cb809e1609
BraidaFac <facundobraida98@gmail.com>
2025-04-01 22:46:51 -0300

    Fixed imprimir comprobante

--- src/comprobante/utils/mapeosEnums.ts
+++ src/comprobante/utils/mapeosEnums.ts
@@ -1,16 +1,16 @@
 export const mapeoTipoComprobante = {
   1: 'FACTURA A',
-  2: 'NOTA DE CRÉDITO A',
-  3: 'NOTA DE DÉBITO A',
+  2: 'NOTA DE DÉBITO A',
+  3: 'NOTA DE CRÉDITO A',
   6: 'FACTURA B',
-  7: 'NOTA DE CRÉDITO B',
-  8: 'NOTA DE DÉBITO B',
+  7: 'NOTA DE DÉBITO B',
+  8: 'NOTA DE CRÉDITO B',
   11: 'FACTURA C',
-  12: 'NOTA DE CRÉDITO C',
-  13: 'NOTA DE DÉBITO C',
+  12: 'NOTA DE DÉBITO C',
+  13: 'NOTA DE CRÉDITO C',
   51: 'FACTURA M',
-  52: 'NOTA DE CRÉDITO M',
-  53: 'NOTA DE DÉBITO M',
+  52: 'NOTA DE DÉBITO M',
+  53: 'NOTA DE CRÉDITO M',
 };
 
 export const mapeoCondicionIVA = {
